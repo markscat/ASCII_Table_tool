f78d993b7aad3f079232955a03e094e5bebb708d
markscat <markscat@gmail.com>
2025-05-12 17:03:44 +0800

    V25.5.12
    初版程式

---
+++ Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace ASCIIT_table
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ListViewAscii = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            columnHeader6 = new ColumnHeader();
+            columnHeader7 = new ColumnHeader();
+            columnHeader8 = new ColumnHeader();
+            columnHeader9 = new ColumnHeader();
+            columnHeader10 = new ColumnHeader();
+            columnHeader11 = new ColumnHeader();
+            columnHeader12 = new ColumnHeader();
+            columnHeader13 = new ColumnHeader();
+            columnHeader14 = new ColumnHeader();
+            columnHeader15 = new ColumnHeader();
+            columnHeader16 = new ColumnHeader();
+            TabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            ASCII2HEX = new TabPage();
+            Lable1 = new Label();
+            ASCII_TextBox = new TextBox();
+            ButTran = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            TabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            ASCII2HEX.SuspendLayout();
+            SuspendLayout();
+            // 
+            // ListViewAscii
+            // 
+            ListViewAscii.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            ListViewAscii.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5, columnHeader6, columnHeader7, columnHeader8, columnHeader9, columnHeader10, columnHeader11, columnHeader12, columnHeader13, columnHeader14, columnHeader15, columnHeader16 });
+            ListViewAscii.FullRowSelect = true;
+            ListViewAscii.GridLines = true;
+            ListViewAscii.Location = new Point(2, 2);
+            ListViewAscii.Margin = new Padding(2);
+            ListViewAscii.Name = "ListViewAscii";
+            ListViewAscii.Size = new Size(953, 599);
+            ListViewAscii.TabIndex = 0;
+            ListViewAscii.UseCompatibleStateImageBehavior = false;
+            ListViewAscii.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Char1";
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Dec1";
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Hex1";
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Oct1";
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Char2";
+            // 
+            // columnHeader6
+            // 
+            columnHeader6.Text = "Dec2";
+            // 
+            // columnHeader7
+            // 
+            columnHeader7.Text = "Hex1";
+            // 
+            // columnHeader8
+            // 
+            columnHeader8.Text = "oct2";
+            // 
+            // columnHeader9
+            // 
+            columnHeader9.Text = "Char3";
+            // 
+            // columnHeader10
+            // 
+            columnHeader10.Text = "DEC3";
+            // 
+            // columnHeader11
+            // 
+            columnHeader11.Text = "Hex3";
+            // 
+            // columnHeader12
+            // 
+            columnHeader12.Text = "Oct3";
+            // 
+            // columnHeader13
+            // 
+            columnHeader13.Text = "char4";
+            // 
+            // columnHeader14
+            // 
+            columnHeader14.Text = "dec4";
+            // 
+            // columnHeader15
+            // 
+            columnHeader15.Text = "Hex4";
+            // 
+            // columnHeader16
+            // 
+            columnHeader16.Text = "Oct4";
+            // 
+            // TabControl1
+            // 
+            TabControl1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            TabControl1.Controls.Add(tabPage1);
+            TabControl1.Controls.Add(ASCII2HEX);
+            TabControl1.Location = new Point(12, 3);
+            TabControl1.Name = "TabControl1";
+            TabControl1.SelectedIndex = 0;
+            TabControl1.Size = new Size(1082, 631);
+            TabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(ListViewAscii);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1074, 603);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "ASCII Table";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // ASCII2HEX
+            // 
+            ASCII2HEX.Controls.Add(label1);
+            ASCII2HEX.Controls.Add(textBox1);
+            ASCII2HEX.Controls.Add(ButTran);
+            ASCII2HEX.Controls.Add(Lable1);
+            ASCII2HEX.Controls.Add(ASCII_TextBox);
+            ASCII2HEX.Location = new Point(4, 24);
+            ASCII2HEX.Name = "ASCII2HEX";
+            ASCII2HEX.Padding = new Padding(3);
+            ASCII2HEX.Size = new Size(1084, 550);
+            ASCII2HEX.TabIndex = 1;
+            ASCII2HEX.Text = "ASCII to HEX";
+            ASCII2HEX.UseVisualStyleBackColor = true;
+            // 
+            // Lable1
+            // 
+            Lable1.AutoSize = true;
+            Lable1.Location = new Point(8, 35);
+            Lable1.Name = "Lable1";
+            Lable1.Size = new Size(36, 15);
+            Lable1.TabIndex = 1;
+            Lable1.Text = "ASCII";
+            // 
+            // ASCII_TextBox
+            // 
+            ASCII_TextBox.Location = new Point(63, 34);
+            ASCII_TextBox.Name = "ASCII_TextBox";
+            ASCII_TextBox.Size = new Size(476, 23);
+            ASCII_TextBox.TabIndex = 0;
+            // 
+            // ButTran
+            // 
+            ButTran.Location = new Point(545, 35);
+            ButTran.Name = "ButTran";
+            ButTran.Size = new Size(112, 23);
+            ButTran.TabIndex = 2;
+            ButTran.Text = "ASCII to HEX";
+            ButTran.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(63, 92);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(476, 264);
+            textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 109);
+            label1.Name = "label1";
+            label1.Size = new Size(31, 15);
+            label1.TabIndex = 4;
+            label1.Text = "HEX";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1087, 634);
+            Controls.Add(TabControl1);
+            Margin = new Padding(2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            TabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            ASCII2HEX.ResumeLayout(false);
+            ASCII2HEX.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView ListViewAscii;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
+        private ColumnHeader columnHeader8;
+        private ColumnHeader columnHeader9;
+        private ColumnHeader columnHeader10;
+        private ColumnHeader columnHeader11;
+        private ColumnHeader columnHeader12;
+        private ColumnHeader columnHeader13;
+        private ColumnHeader columnHeader14;
+        private ColumnHeader columnHeader15;
+        private ColumnHeader columnHeader16;
+        private TabControl TabControl1;
+        private TabPage tabPage1;
+        private TabPage ASCII2HEX;
+        private Label Lable1;
+        private TextBox ASCII_TextBox;
+        private TextBox textBox1;
+        private Button ButTran;
+        private Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Windows.Forms; // 確保引用 System.Windows.Forms
+
+
+
+
+namespace ASCIIT_table
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //PopulateAsciiTable();
+            PopulateAsciiTableGrouped();
+        }
+        // 輔助方法：取得要顯示的字元 (處理控制字元)
+        private string GetDisplayChar(int asciiCode)
+        {
+            char character = (char)asciiCode;
+            if (asciiCode == 32) return "(Space)";
+            // 可以加入其他特殊字元處理，如 Tab, LF, CR
+            // else if (asciiCode == 9) return "(Tab)";
+            // else if (asciiCode == 10) return "(LF)";
+            // else if (asciiCode == 13) return "(CR)";
+            else if (char.IsControl(character) || asciiCode == 127) // 包含 DEL
+            {
+                // 可以返回 ASCII 控制字元名稱，或統一符號
+                // 簡單起見，返回 "."
+                return ".";
+            }
+            else
+            {
+                return character.ToString();
+            }
+        }
+
+
+
+        private void PopulateAsciiTableGrouped()
+        {
+
+            ListViewAscii.Items.Clear();
+            ListViewAscii.BeginUpdate();
+
+            const int groupSize = 32;
+            //const int numGroups = 4; // 我們要顯示四組
+
+            // 迴圈跑 32 次，代表 32 列
+            for (int i = 0; i < groupSize; i++)
+            {
+                // 計算每一組對應的 ASCII 基礎值
+                int asciiVal1 = i;                  // 組 1: 0-31
+                int asciiVal2 = i + groupSize;      // 組 2: 32-63
+                int asciiVal3 = i + (groupSize * 2); // 組 3: 64-95
+                int asciiVal4 = i + (groupSize * 3); // 組 4: 96-127
+
+                // 建立 ListViewItem，第一欄是第一組的 Char
+                ListViewItem item = new ListViewItem(GetDisplayChar(asciiVal1));
+
+                // --- 加入第一組的 Dec, Hex, Oct ---
+                item.SubItems.Add(asciiVal1.ToString());                // Dec 1
+                item.SubItems.Add(asciiVal1.ToString("X2"));            // Hex 1
+                item.SubItems.Add(Convert.ToString(asciiVal1, 8).PadLeft(3, '0')); // Oct 1
+
+                // --- 加入第二組的 Char, Dec, Hex, Oct ---
+                item.SubItems.Add(GetDisplayChar(asciiVal2));           // Char 2
+                item.SubItems.Add(asciiVal2.ToString());                // Dec 2
+                item.SubItems.Add(asciiVal2.ToString("X2"));            // Hex 2
+                item.SubItems.Add(Convert.ToString(asciiVal2, 8).PadLeft(3, '0')); // Oct 2
+
+                // --- 加入第三組的 Char, Dec, Hex, Oct ---
+                item.SubItems.Add(GetDisplayChar(asciiVal3));           // Char 3
+                item.SubItems.Add(asciiVal3.ToString());                // Dec 3
+                item.SubItems.Add(asciiVal3.ToString("X2"));            // Hex 3
+                item.SubItems.Add(Convert.ToString(asciiVal3, 8).PadLeft(3, '0')); // Oct 3
+
+                // --- 加入第四組的 Char, Dec, Hex, Oct ---
+                item.SubItems.Add(GetDisplayChar(asciiVal4));           // Char 4
+                item.SubItems.Add(asciiVal4.ToString());                // Dec 4
+                item.SubItems.Add(asciiVal4.ToString("X2"));            // Hex 4
+                item.SubItems.Add(Convert.ToString(asciiVal4, 8).PadLeft(3, '0')); // Oct 4
+
+                // 將完整的一列加入 ListView
+                ListViewAscii.Items.Add(item);
+            }
+
+            ListViewAscii.EndUpdate();
+
+            // (可選) 自動調整欄寬，但對於這麼多欄位可能效果不佳，手動設定較好
+            // listViewAscii.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+        }
+
+    }
+#if ascii_list
+        private void PopulateAsciiTable()
+        {
+            // 清除 ListView 中可能存在的舊項目 (如果需要重新載入)
+            listViewAscii.Items.Clear();
+
+            // 設定 ListView 開始更新，提高效能
+            listViewAscii.BeginUpdate();
+
+            // 迴圈處理標準 ASCII (0-127) 或 擴充 ASCII (0-255)
+            // 這裡以 0-127 為例
+            for (int i = 0; i <= 127; i++)
+            {
+                // 取得字元表示
+                char character = (char)i;
+                string displayChar;
+
+                // 特殊處理控制字元 (0-31 和 127)
+                if (i == 32) // 空白字元特別標示
+                {
+                    displayChar = "(Space)";
+                }
+                // 其他常見的可見空白字元 (可選)
+                // else if (i == 9) displayChar = "(Tab)";
+                // else if (i == 10) displayChar = "(LF)";
+                // else if (i == 13) displayChar = "(CR)";
+                else if (char.IsControl(character))
+                {
+                    // 對於其他控制字元，顯示一個點或其他標記符
+                    // 或者你可以查詢 ASCII 控制字元的名稱 (例如 NUL, SOH, STX...)
+                    displayChar = ".";
+                }
+                else
+                {
+                    // 可顯示的字元直接轉換
+                    displayChar = character.ToString();
+                }
+
+                // 建立 ListViewItem (代表表格中的一列)
+                // 第一個欄位 (Char) 的文字直接設定給 ListViewItem 的 Text 屬性
+                ListViewItem item = new ListViewItem(displayChar);
+
+                // 加入後續欄位的子項目 (SubItems)
+                // Dec (十進位)
+                item.SubItems.Add(i.ToString());
+
+                // Hex (十六進位)，使用 "X2" 格式確保至少兩位數，並補零
+                item.SubItems.Add(i.ToString("X2"));
+
+                // Oct (八進位)，使用 Convert.ToString 並左邊補零到三位數
+                item.SubItems.Add(Convert.ToString(i, 8).PadLeft(3, '0'));
+
+                // 將這一列項目加入到 ListView
+                listViewAscii.Items.Add(item);
+            }
+
+            // 設定 ListView 結束更新，一次性顯示所有內容
+            listViewAscii.EndUpdate();
+
+            // (可選) 自動調整欄寬以適應內容或標頭
+            // listViewAscii.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+            // listViewAscii.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+#endif
+
+}
+
